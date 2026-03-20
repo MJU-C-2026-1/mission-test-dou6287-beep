6353a3d09c5f2ec68d8773a1770c341561598bce
dou6287-beep <dou6287@mju.ac.kr>
2026-03-20 22:33:01 +0900

    Hellow World 작성 완료

--- main.c
+++ main.c
@@ -2,34 +2,10 @@
   파일이름:
   작 성 자:김도우
 */
-import pygame
-import sys
+#include <stdio.h>
 
-class AlienInvasion:
-  """Settings for Alien Invasion Game"""
-
-  def__init__(self):
-     """Initializing the gmae settings"""
-     pygame.init()
-
-     self.screen = pygame.display.set_mode((1200, 800))
-     pygame.display.set_caption("Alien_Invasion")
-
-     self.bg_color = (200, 0, 10)
-
-
-  def run_game(self):
-      while True:
-          for enent in pygame.event.get():
-              if event.type == pygame.QUIT:
-                  sys.exit()
-
-      slef.screen.fill(self.bg_color)
-
-      pygame.display.flip()
-
-if __name__ == '__main__':
-    ai = AlienInvasion()
-    ai.run_game()
-
-  
+int main()
+{
+  printf("Hello World");
+  return 0;
+}
